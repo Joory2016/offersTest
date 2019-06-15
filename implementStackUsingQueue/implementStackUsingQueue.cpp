0a209c0f62e4a83c9ff13917df864ef51c57a80a
ZhaoRui <jooryde@yeah.net>
2019-06-15 23:11:01 +0800

    add the test for using queue to implement a stack. using an additional queue as temp queue.

---
+++ implementStackUsingQueue/implementStackUsingQueue.cpp
@@ -0,0 +1,88 @@
+﻿// implementStackUsingQueue.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class MyStack
+{
+public:
+	/** Initialize your data structure here. */
+	MyStack()
+	{
+	}
+
+	/** Push element x onto stack. */
+	void push(int x)
+	{
+		if (q.empty())
+		{
+			q.push(x);
+			return;
+		}
+		queue<int> temp;
+		temp.push(x);
+		while (!q.empty())
+		{
+			temp.push(q.front());
+			q.pop();
+		}
+		while (!temp.empty())
+		{
+			q.push(temp.front());
+			temp.pop();
+		}
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	int pop()
+	{
+		int temp = q.front();
+		q.pop();
+		return temp;
+	}
+
+	/** Get the top element. */
+	int top()
+	{
+		return q.front();
+	}
+
+	/** Returns whether the stack is empty. */
+	bool empty()
+	{
+		if (q.empty())
+			return true;
+		return false;
+	}
+
+private:
+	queue<int> q;
+};
+
+
+int main()
+{
+	MyStack s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	int temp = s.top();
+
+
+    std::cout << "Hello World!\n"; 
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
